c8146e11cf7102228cb9de4ecaa89aa06ed7382f
Mbasa <mbasacokile7@gmail.com>
2026-02-25 20:06:50 +0200

    Feat: Add Item append functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import Form from "./Components/Form";
+import { nanoid } from "nanoid";
 
 const App = () => {
   // Items State Variable
   const [items, setItems] = useState([]);
+
+  // AddItem Function
+  function addItem(itemName) {
+    const itemId = nanoid();
+    const item = { id: itemId, name: itemName, completed: false };
+    setItems([...items, item]);
+  }
   return (
-    <section>
-      <Form />
+    <section className="section-center">
+      <Form addItem={addItem} />
     </section>
   );
 };
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,13 +1,38 @@
-function Form() {
+import { nanoid } from "nanoid";
+import { useState } from "react";
+
+function Form({ addItem }) {
+  // Create a state value for the item
+  const [newItemName, setNewItemName] = useState("");
   //handle submit function
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("Button was clicked cuz");
+    // If something is written
+    if (newItemName) {
+      // Add item when something is written
+      addItem(newItemName);
+      // reset the newItemName state value
+      setNewItemName("");
+      // TODO: Add Success Toast
+    } else {
+      // TODO: Add Error Toast
+      console.log("Nothing was typed");
+    }
   }
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" />
-      <button>Add Item</button>
+      <h4>Grocery bud</h4>
+      <div className="form-control">
+        <input
+          type="text"
+          className="form-input"
+          value={newItemName}
+          onChange={(e) => setNewItemName(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Add Item
+        </button>
+      </div>
     </form>
   );
 }
